cfd9afb8b188e3c0ea3bdd56725f62222cce2c8d
Mahmoud Elshahatt <79477855+MahmoudElshahatt@users.noreply.github.com>
2022-11-20 22:21:08 +0200

    Add files via upload

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a[5];
+
+    for(int i=0; i<5; i++)
+    {
+        cin>>a[i];
+    }
+    int n=a.size();
+    for(int outer=0; outer<n-1; outer++)
+    {
+        for(int inner=0; inner<n-outer-1; inner++)
+        {
+            if(a[inner]>a[inner+1])
+            {
+                swap(a[inner],a[inner+1]);
+            }
+        }
+    }
+
     return 0;
 }
